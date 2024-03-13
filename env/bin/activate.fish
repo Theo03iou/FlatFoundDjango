c5e1e824225caf097625515b5687b3d335a83688
Theo Georgiou <75097082+Theo03iou@users.noreply.github.com>
2024-03-13 19:51:57 +0000

    Setup

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/theogeorgiou/Desktop/FlatFound/env"
+set -gx VIRTUAL_ENV "/Users/theogeorgiou/Desktop/FlatFoundDjango/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
