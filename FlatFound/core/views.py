8de287dda8ce50507b5b2ccb0392a2f58aaec11d
Theo Georgiou <75097082+Theo03iou@users.noreply.github.com>
2024-03-16 12:17:29 +0000

    Signup

--- FlatFound/core/views.py
+++ FlatFound/core/views.py
@@ -1,6 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from property.models import Category, Property
+
+from .forms import SignupForm
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'core/index.html')
+    properties = Property.objects.all()[0:6]
+    categories = Category.objects.all
+    
+    return render(request, 'core/index.html', {
+        'categories': categories,
+        'properties': properties
+    })
+
+def contact(request):
+    return render(request, 'core/contact.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            return redirect('/login/')
+    else:
+        form = SignupForm()
+    
+    return render(request, 'core/signup.html', {
+        'form': form
+    })
