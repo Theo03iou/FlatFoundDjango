a64190de4d1d88ddd24269440b6299625a4860f9
Theo Georgiou <75097082+Theo03iou@users.noreply.github.com>
2024-03-21 11:39:46 +0000

    Search feature

--- FlatFound/property/views.py
+++ FlatFound/property/views.py
@@ -14,6 +14,13 @@ def property(request):
     length_type = Category.objects.all()
     properties = Property.objects.all()
 
+    if length_id:
+        properties = properties.filter(length_id=length_id)
+
+    if query:
+        properties = properties.filter(
+            Q(postcode__icontains=query) | Q(description__icontains=query))
+
     return render(request, 'property/listings.html', {
         'properties': properties,
         'query': query,
@@ -24,53 +31,57 @@ def property(request):
 
 def detail(request, pk):
     property = get_object_or_404(Property, pk=pk)
-    related_properties = Property.objects.filter(category=property.category).exclude(pk=pk)[0:3]
+    related_properties = Property.objects.filter(
+        category=property.category).exclude(pk=pk)[0:3]
     return render(request, 'property/detail.html', {
         'property': property,
-        'related_properties': related_properties  
+        'related_properties': related_properties
     })
-    
+
+
 @login_required
 def new(request):
     if request.method == 'POST':
         form = NewListingForm(request.POST, request.FILES)
-        
+
         if form.is_valid():
             property = form.save(commit=False)
             property.created_by = request.user
             property.save()
-            
+
             return redirect('property:detail', pk=property.id)
     else:
         form = NewListingForm()
-    
+
     return render(request, 'property/form.html', {
         'form': form,
         'title': 'New Property',
-        })
+    })
+
 
 @login_required
 def edit(request, pk):
     property = get_object_or_404(Property, pk=pk, created_by=request.user)
-    
+
     if request.method == 'POST':
         form = EditListingForm(request.POST, request.FILES, instance=property)
-        
+
         if form.is_valid():
             form.save()
-            
+
             return redirect('property:detail', pk=property.id)
     else:
         form = EditListingForm(instance=property)
-        
+
     return render(request, 'property/form.html', {
         'form': form,
         'title': 'Edit property',
     })
-    
+
+
 @login_required
 def delete(request, pk):
     property = get_object_or_404(Property, pk=pk, created_by=request.user)
     property.delete()
-    
+
     return redirect('dashboard:index')
