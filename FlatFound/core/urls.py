42677138222301be1a86244ffd209460220ec960
Theo Georgiou <75097082+Theo03iou@users.noreply.github.com>
2024-04-02 13:48:09 +0100

    Small things
    Changed login to the homepage, allowed users to make an account from login

--- FlatFound/core/urls.py
+++ FlatFound/core/urls.py
@@ -7,10 +7,13 @@ from .forms import LoginForm
 app_name = 'core'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', auth_views.LoginView.as_view(template_name='core/login.html',
+         authentication_form=LoginForm),  name='login'),
     path('contact/', views.contact, name='contact'),
     path('signup/', views.signup, name='signup'),
-    path('login/', auth_views.LoginView.as_view(template_name='core/login.html',
-         authentication_form=LoginForm),  name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='core/logout.html', next_page=None), name='logout')
+    path('index/', views.index, name='index'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='core/logout.html',
+         next_page=None), name='logout')
 ]
+
+# Attempting to make the login screen the default
