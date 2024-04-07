86eb3217863778cbf1505f0842dd0f5b3df53c44
Theo Georgiou <75097082+Theo03iou@users.noreply.github.com>
2024-04-07 16:28:15 +0100

    added more settings and fixed detail page

--- FlatFound/property/forms.py
+++ FlatFound/property/forms.py
@@ -7,14 +7,12 @@ INPUT_CLASSES = 'w-full py-4 px-6 rounded-xl border'
 class NewListingForm(forms.ModelForm):
     class Meta:
         model = Property
-        fields = ('category', 'property_type', 'price_pcm', 'ppw', 'address', 'postcode', 'num_beds', 'num_bathrooms', 'description', 'image',) 
+        fields = ('category',  'price_pcm', 'ppw', 'address', 'postcode', 'pet_friendly', 'number_of_beds', 'number_of_bathrooms', 'description', 'image',) 
         widgets = {
             'category': forms.Select(attrs={
                 'class': INPUT_CLASSES
         }),
-            'property_type': forms.Select(attrs={
-                'class': INPUT_CLASSES
-        }),
+            
             'price_pcm': forms.TextInput(attrs={
                 'class': INPUT_CLASSES
         }),
@@ -27,10 +25,13 @@ class NewListingForm(forms.ModelForm):
             'postcode': forms.TextInput(attrs={
                 'class': INPUT_CLASSES
         }),
-            'num_beds': forms.IntegerField(attrs={
+            'pet_friendly': forms.CheckboxInput(attrs={
                 'class': INPUT_CLASSES
         }),
-            'num_bathrooms': forms.IntegerField(attrs={
+            'number_of_beds': forms.TextInput(attrs={
+                'class': INPUT_CLASSES
+        }),
+            'number_of_bathrooms': forms.TextInput(attrs={
                 'class': INPUT_CLASSES
         }),
             'description': forms.Textarea(attrs={
@@ -44,7 +45,7 @@ class NewListingForm(forms.ModelForm):
 class EditListingForm(forms.ModelForm):
     class Meta:
         model = Property
-        fields = ('category', 'property_type', 'price_pcm', 'ppw', 'address', 'postcode', 'num_beds', 'num_bathrooms', 'description', 'image',) 
+        fields = ('category',  'price_pcm', 'ppw', 'address', 'postcode', 'pet_friendly', 'number_of_beds', 'number_of_bathrooms', 'description', 'image',) 
         widgets = {
             'category': forms.Select(attrs={
                 'class': INPUT_CLASSES
@@ -61,14 +62,17 @@ class EditListingForm(forms.ModelForm):
             'postcode': forms.TextInput(attrs={
                 'class': INPUT_CLASSES
         }),
-            'num_beds': forms.IntegerField(attrs={
+            'pet_friendly': forms.CheckboxInput(attrs={
                 'class': INPUT_CLASSES
         }),
-            'num_bathrooms': forms.IntegerField(attrs={
+            'number_of_beds': forms.TextInput(attrs={
+                'class': INPUT_CLASSES
+        }),
+            'number_of_bathrooms': forms.TextInput(attrs={
                 'class': INPUT_CLASSES
         }),
             'description': forms.Textarea(attrs={
-                'class': INPUT_CLASSES
+                'class': INPUT_CLASSES 
         }),
             'image': forms.FileInput(attrs={
                 'class': INPUT_CLASSES
