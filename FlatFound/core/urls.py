71ff332de195486c9d42241f976430e7c354a806
Theo Georgiou <75097082+Theo03iou@users.noreply.github.com>
2024-04-07 21:36:35 +0100

    Revert "Merge branch 'main' of https://github.com/Theo03iou/FlatFoundDjango"
    This reverts commit f05f78d9ea79c05d466e3cc1fa5c748a0b9d4870, reversing
    changes made to 6e22054b4f495eb729f5c53bc77ebde9121563e3.

--- FlatFound/core/urls.py
+++ FlatFound/core/urls.py
@@ -7,15 +7,13 @@ from .forms import LoginForm
 app_name = 'core'
 
 urlpatterns = [
-    path('', auth_views.LoginView.as_view(template_name='core/index.html',
-         authentication_form=LoginForm),  name='login'),
-    path('login/', auth_views.LoginView.as_view(template_name='core/index.html',
+    path('', auth_views.LoginView.as_view(template_name='core/login.html',
          authentication_form=LoginForm),  name='login'),
     path('contact/', views.contact, name='contact'),
     path('signup/', views.signup, name='signup'),
     path('index/', views.index, name='index'),
     path('logout/', auth_views.LogoutView.as_view(template_name='core/logout.html',
-         next_page='core/index.html'), name='logout')
+         next_page=None), name='logout')
 ]
 
 # Attempting to make the login screen the default
--- FlatFound/property/forms.py
+++ FlatFound/property/forms.py
@@ -7,12 +7,11 @@ INPUT_CLASSES = 'w-full py-4 px-6 rounded-xl border'
 class NewListingForm(forms.ModelForm):
     class Meta:
         model = Property
-        fields = ('category',  'price_pcm', 'ppw', 'address', 'postcode', 'pet_friendly', 'number_of_beds', 'number_of_bathrooms', 'description', 'image',) 
+        fields = ('category', 'price_pcm', 'ppw', 'address', 'postcode', 'pet_friendly', 'num_beds', 'description', 'image',) 
         widgets = {
             'category': forms.Select(attrs={
                 'class': INPUT_CLASSES
         }),
-            
             'price_pcm': forms.TextInput(attrs={
                 'class': INPUT_CLASSES
         }),
@@ -28,10 +27,7 @@ class NewListingForm(forms.ModelForm):
             'pet_friendly': forms.CheckboxInput(attrs={
                 'class': INPUT_CLASSES
         }),
-            'number_of_beds': forms.TextInput(attrs={
-                'class': INPUT_CLASSES
-        }),
-            'number_of_bathrooms': forms.TextInput(attrs={
+            'num_beds': forms.TextInput(attrs={
                 'class': INPUT_CLASSES
         }),
             'description': forms.Textarea(attrs={
@@ -45,7 +41,7 @@ class NewListingForm(forms.ModelForm):
 class EditListingForm(forms.ModelForm):
     class Meta:
         model = Property
-        fields = ('category',  'price_pcm', 'ppw', 'address', 'postcode', 'pet_friendly', 'number_of_beds', 'number_of_bathrooms', 'description', 'image',) 
+        fields = ('category', 'price_pcm', 'ppw', 'address', 'postcode', 'pet_friendly', 'num_beds', 'description', 'image',) 
         widgets = {
             'category': forms.Select(attrs={
                 'class': INPUT_CLASSES
@@ -65,14 +61,11 @@ class EditListingForm(forms.ModelForm):
             'pet_friendly': forms.CheckboxInput(attrs={
                 'class': INPUT_CLASSES
         }),
-            'number_of_beds': forms.TextInput(attrs={
-                'class': INPUT_CLASSES
-        }),
-            'number_of_bathrooms': forms.TextInput(attrs={
+            'num_beds': forms.TextInput(attrs={
                 'class': INPUT_CLASSES
         }),
             'description': forms.Textarea(attrs={
-                'class': INPUT_CLASSES 
+                'class': INPUT_CLASSES
         }),
             'image': forms.FileInput(attrs={
                 'class': INPUT_CLASSES
